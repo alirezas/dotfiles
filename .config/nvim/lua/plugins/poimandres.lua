20f9b8fef0f14afac07aca26d8bb1fb1511275ba
Alireza Sarabchi <a.sarabchi@gmail.com>
2024-10-30 23:58:42 +0330

    nvim configs

--- .config/nvim/lua/plugins/poimandres.lua
+++ .config/nvim/lua/plugins/poimandres.lua
@@ -1,17 +1,16 @@
-return { 
-  'olivercederborg/poimandres.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require('poimandres').setup {
-      -- leave this setup function empty for default config
-      -- or refer to the configuration section
-      -- for configuration options
-    }
-  end,
+return {
+	"olivercederborg/poimandres.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("poimandres").setup({
+			disable_background = true,
+			disable_float_background = true,
+		})
+	end,
 
-  -- optionally set the colorscheme within lazy config
-  init = function()
-    vim.cmd("colorscheme poimandres")
-  end
+	-- optionally set the colorscheme within lazy config
+	init = function()
+		vim.cmd("colorscheme poimandres")
+	end,
 }
