aa199ffec20b882ee0c3933286cba1e299b0bb35
Alireza Sarabchi <a.sarabchi@gmail.com>
2025-03-24 12:13:39 +0330

    Refactor front_app item in SketchyBar configuration by removing redundant chevron item addition for improved clarity and simplicity.

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -4,9 +4,7 @@
 # We add some regular items to the left side of the bar, where
 # only the properties deviating from the current defaults need to be set
 
-sketchybar --add item chevron left \
-  --set chevron icon= label.drawing=off \
-  --add item front_app left \
+sketchybar --add item front_app left \
   --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
   background.color=0x40000000 \
   background.corner_radius=25 \
